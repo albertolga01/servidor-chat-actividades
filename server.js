5e738e00e5f8c62c0735b6567afb7fdd0dba7736
albertolga01 <67989767+albertolga01@users.noreply.github.com>
2023-07-22 11:09:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${ port }`);
+    curl http://127.0.0.1:3000
 });
 
 const io = new Server(server, {
