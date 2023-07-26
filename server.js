d6aeae9b0158030f662252322959c63db0ecdfff
albertolga01 <alberto.lga01@gmail.com>
2023-07-26 09:58:27 -0700

    202307267

--- server.js
+++ server.js
@@ -8,15 +8,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const httpProxy = require("http-proxy");
-
-httpProxy
-  .createProxyServer({
-    target: "http://localhost:3000",
-    ws: true,
-  })
-  .listen(5000);
-
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${ port }`);
 });
