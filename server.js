3a46d156fb6f019f7ccb2aded8cd1e3150552e46
albertolga01 <alberto.lga01@gmail.com>
2023-07-26 10:26:53 -0700

    2023072610

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const server = app.listen(port, () => {
+const server = app.listen(port, '0.0.0.0',() => {
     console.log(`Server is running on port ${ port }`);
 });
 
